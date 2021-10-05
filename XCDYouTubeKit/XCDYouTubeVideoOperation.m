8b4d084e9d3f4dc4c9ab66ae513b35d41137753c
Kirill Pahnev <kirill.pahnev@gmail.com>
2021-10-05 09:25:11 +0300

    Another query fix

--- XCDYouTubeKit/XCDYouTubeVideoOperation.m
+++ XCDYouTubeKit/XCDYouTubeVideoOperation.m
@@ -149,18 +149,7 @@ static NSError *YouTubeError(NSError *error, NSSet *regionsAllowed, NSString *la
 		NSString *eventLabel = [self.eventLabels objectAtIndex:0];
 		[self.eventLabels removeObjectAtIndex:0];
 		
-		NSDictionary *query = @{
-			@"video_id": self.videoIdentifier,
-			@"hl": self.languageIdentifier,
-			@"el": eventLabel,
-			@"ps": @"default",
-			@"html5" : @"1",
-			@"eurl": [@"https://youtube.googleapis.com/v/" stringByAppendingString:self.videoIdentifier],
-			@"c": @"TVHTML5",
-			@"cver": @"6.20180913"
-		};
-		NSString *queryString = XCDQueryStringWithDictionary(query);
-		NSURL *videoInfoURL = [NSURL URLWithString:[@"https://www.youtube.com/get_video_info?" stringByAppendingString:queryString]];
+        NSURL *videoInfoURL = [NSURL URLWithString:[@"https://www.youtube.com/embed/" stringByAppendingString:self.videoIdentifier]];
 		[self startRequestWithURL:videoInfoURL type:XCDYouTubeRequestTypeGetVideoInfo];
 	}
 }
@@ -365,11 +354,7 @@ static NSError *YouTubeError(NSError *error, NSSet *regionsAllowed, NSString *la
 	
 	if (self.webpage.isAgeRestricted && self.cookies.count == 0)
 	{
-		NSString *eurl = [@"https://youtube.googleapis.com/v/" stringByAppendingString:self.videoIdentifier];
-		NSString *sts = self.embedWebpage.sts ?: self.webpage.sts ?: @"";
-		NSDictionary *query = @{ @"video_id": self.videoIdentifier, @"hl": self.languageIdentifier, @"eurl": eurl, @"sts": sts};
-		NSString *queryString = XCDQueryStringWithDictionary(query);
-		NSURL *videoInfoURL = [NSURL URLWithString:[@"https://www.youtube.com/get_video_info?" stringByAppendingString:queryString]];
+        NSURL *videoInfoURL = [NSURL URLWithString:[@"https://www.youtube.com/embed/" stringByAppendingString:self.videoIdentifier]];
 		[self startRequestWithURL:videoInfoURL type:XCDYouTubeRequestTypeGetVideoInfo];
 	}
 	else
